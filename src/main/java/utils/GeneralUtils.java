4c63a3e84ec8af60b6aef14719b495f143ea1acf
David Rudenko <drudenko@drbsystems.com>
2021-05-17 00:30:56 +0300

    Fixed WIndows problem with app maximazing and taskbar icon

--- src/main/java/utils/GeneralUtils.java
+++ src/main/java/utils/GeneralUtils.java
@@ -1,10 +1,13 @@
 package utils;
 
+import com.sun.javafx.PlatformUtil;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 
@@ -18,12 +21,19 @@ public class GeneralUtils {
         Parent root = FXMLLoader.load(GeneralUtils.class.getResource(docPath));
         Stage stage = new Stage();
         stage.setTitle("Quizzer");
-        stage.setScene(new Scene(root, 1920, 1080));
-        stage.setMaximized(true);
-        stage.setResizable(false);
+        stage.setScene(new Scene(root));
+
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+        stage.setX(primaryScreenBounds.getMinX());
+        stage.setY(primaryScreenBounds.getMinY());
+        stage.setWidth(primaryScreenBounds.getWidth());
+        stage.setHeight(primaryScreenBounds.getHeight());
+
         var iconImagePath = "/assets/quiz-icon.png";
         stage.getIcons().add(new Image(GeneralUtils.class.getResourceAsStream(iconImagePath)));
-        Taskbar.getTaskbar().setIconImage(Toolkit.getDefaultToolkit().getImage(GeneralUtils.class.getResource(iconImagePath)));
+        if (PlatformUtil.isMac()) {
+            Taskbar.getTaskbar().setIconImage(Toolkit.getDefaultToolkit().getImage(GeneralUtils.class.getResource(iconImagePath)));
+        }
         stage.show();
         if (prevWindow != null) {
             prevWindow.hide();
